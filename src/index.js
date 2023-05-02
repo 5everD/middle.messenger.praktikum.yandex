7175c861f21f76f40f43f4888c973f56fd34eb58
Vsevolod <demidov.vsevolod@uep24.ru>
2023-05-03 00:36:05 +0300

    style fix and prepare for netlify

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import main from './pages/main/main.hbs';
-
-window.addEventListener('load', () => {
-    const rootNode = document.getElementById('root');
-    console.log(main)
-    rootNode.innerHTML = main({});
-})
